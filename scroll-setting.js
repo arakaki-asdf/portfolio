3b523abb7ca38e443a0af7aa3a954543bb957154
arakaki hayate <hayateasdf@gmail.com>
2024-08-05 19:35:09 +0900

    スクロール時のアニメーション追加 skill文言修正

---
+++ scroll-setting.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+    // スクロールアニメーション
+    ScrollReveal().reveal('.aboutme', {
+        duration: 800,
+        viewFactor: 0.2,
+        reset: false,
+        distance: '20%',
+        scale: 1,
+        delay: 200,
+        opacity: 0,
+        origin: 'bottom',
+    });
+
+    ScrollReveal().reveal('.item', {
+        delay: 200,
+        duration: 400,
+        origin: 'left',
+        distance: '50px',
+        interval: 20,
+        reset: false,
+    });
+
+    ScrollReveal().reveal('.skill', {
+        delay: 300,
+        duration: 800,
+        viewFactor: 0.2,
+        origin: 'left',
+        distance: '50px',
+        interval: 30,
+        reset: false,
+    });
+
+    ScrollReveal().reveal('#promotion', {
+        delay: 300,
+        duration: 800,
+        viewFactor: 0.2,
+        opacity: 0,
+        reset: false,
+    });
+    ScrollReveal().reveal('#history', {
+        delay: 200,
+        duration: 800,
+        viewFactor: 0.2,
+        opacity: 0,
+        reset: false,
+    });
+};
