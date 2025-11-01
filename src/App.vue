4073fac65872d0db0e25cd2e1d3c4219b42bdcc9
red-hot-chili-pepper <hayateasdf@gmail.com>
2025-11-01 10:12:52 +0900

    Revert "Updates"
    This reverts commit 0c0d9fc958e7daeeec1508980c55edc602526980.

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+<template>
+  <header>
+    <nav>
+      <a href="#production">PRODUCTION</a>
+      <a href="#skills">SKILLS</a>
+      <a href="#promotion">PROMOTION</a>
+      <a href="#history">HISTORY</a>
+    </nav>
+
+    <div id="logo">
+      <img src="@/assets/images/dog.jpg">
+    </div>
+  </header>
+
+  <main>
+    <!-- <RouterView /> -->
+    <Profile />
+    <Production />
+    <Skills />
+    <Promotion />
+    <History />
+  </main>
+
+  <footer>
+    <a href="#">TOP</a>
+    <p class="copyright">&copy; 2025 Arakaki Hayate</p>
+  </footer>
+</template>
+
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router';
+import Profile from './components/Profile.vue';
+import Promotion from './components/Promotion.vue';
+import Production from './components/Production.vue';
+import Skills from './components/Skills.vue';
+import History from './components/History.vue';
+
+window.onload = () => {
+    // スクロールアニメーション
+    ScrollReveal().reveal('.aboutme', {
+        duration: 800,
+        reset: false,
+        distance: '20%',
+        scale: 1,
+        delay: 200,
+        origin: 'bottom',
+    });
+    ScrollReveal().reveal('.item', {
+        delay: 200,
+        duration: 400,
+        origin: 'left',
+        distance: '50px',
+        interval: 20,
+        reset: false,
+    });
+    ScrollReveal().reveal('.skill', {
+        delay: 300,
+        duration: 800,
+        viewFactor: 0.2,
+        origin: 'left',
+        distance: '50px',
+        interval: 30,
+        reset: false,
+    });
+    ScrollReveal().reveal('#promotion', {
+        delay: 300,
+        duration: 800,
+        viewFactor: 0.2,
+        opacity: 0,
+        reset: false,
+    });
+    ScrollReveal().reveal('#history', {
+        delay: 200,
+        duration: 800,
+        viewFactor: 0.2,
+        opacity: 0,
+        reset: false,
+    });
+};
+</script>
+
+<style>
+header {
+  margin: 0;
+
+  nav {
+    display: grid;
+    grid-auto-flow: column;
+    gap: 12px;
+    padding: 0 10px;
+    justify-content: right;
+    font-family: var(--sub-font-family);
+    font-size: 14px;
+    /* margin-top: 2rem; */
+
+    a {
+      padding: 8px 0;
+      margin: 0px;
+    }
+  }
+
+  #logo {
+    img {
+      width: 100%;
+      height: 400px;
+
+      background: url(@/assets/images/dog.jpg) center / cover;
+      object-fit: cover;
+    }
+  }
+}
+
+main {
+  display: grid;
+  justify-content: center;
+  padding: 20px 10px;
+  gap: 40px 0;
+}
+
+footer {
+  margin: 0;
+  padding: 0 10px;
+
+  a {
+    font-family: var(--sub-font-family);
+    font-size: 14px;
+  }
+
+  .copyright {
+    display: inline-block;
+    margin-left: 12px;
+    font-size: 12px;
+    color: var(--light-color);
+  }
+}
+
+@container (width < 700px) {
+  header {
+    #logo {
+      img {
+        height: 200px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="about">
+    <h1>Aboue Me</h1>
+    <p>Here you can write aboute yourself and your skills.</p>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+
+<style scoped>
+  .about {
+    padding: 20px;
+    text-align: center;
+  }
+</style>
